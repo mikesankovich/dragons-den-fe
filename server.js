dafb55e99b4483ffc1fcf1bf2bb3b616a8ecf080
Michael Sankovich <misankovich@gmail.com>
2020-01-26 03:00:03 -0500

    server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(path.join(__dirname, '/dist/dnd-api')));
+// Start the app by listening on the default
+// Heroku port
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/dnd-api/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
